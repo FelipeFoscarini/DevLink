7b785c973e6a5453a269051934b4cbbd5577c17a
FelipeFoscarini <felipeoscarini@gmail.com>
2023-02-02 22:25:29 -0300

    Add Function jsChoseImgProfile

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function toggleMode() {
     const html = document.documentElement
+    html.classList.toggle("light")
 
-  //  if(html.classList.contains('light')){
-  //   html.classList.remove('light')
-  //  } else {
-  //      html.classList.add('light')
-  //  }
+    //Seleciona a tag img
+    const img = document.querySelector('#profile img')
 
-  html.classList.toggle("light")
+
+           //Substituir a img
+     if(html.classList.contains("light")) {   
+          //se tiver a class light, adicionar imagem light
+        img.setAttribute("src","./assets/avatarLight.png")
+          //altera o alt caso tenha a class light
+        img.setAttribute("alt", "Sem imagem de perfil")
+  
+     } else{
+            //ou se tiver sem light mode, manter a imagem normal
+       img.setAttribute("src", "./assets/avatar.png")
+            //altera o alt caso não tenha a class light
+       img.setAttribute("alt", "Foto perfil felipe")
+     }
 }
